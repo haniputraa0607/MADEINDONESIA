1611bf1b65610969ec3f0a3874257ae07c38fcb5
haniputraa0607 <hanifmurtadha0607@gmail.com>
2021-08-17 17:58:02 +0700

    membuat database koneksi

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+  $hostname = 'localhost';
+  $username = 'root';
+  $password = 12345678;
+  $database = 'madeindonesia';
+
+  $db = new mysqli($hostname, $username, $password, $database);
+  if($db->connect_errno){
+    die('Error ' . $this->db->connect_error);
+  }
+  
+  // include_once('UserClass.php');
+  // $con = new UserClass($db);
+?>
